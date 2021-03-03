7ed84f8b640042b8f2e68af95622f28b556f8665
NicholasKao <kao.nicholas@gmail.com>
2021-03-02 20:22:12 -0600

    initial list of healthy transformations and a fucntional healthy_transform method

---
+++ nick_transform.py
@@ -0,0 +1,41 @@
+import json
+import Levenshtein
+
+with open('healthy_transforms.json') as f:
+    transforms = json.load(f)
+
+
+def healthy_transform(ingredient):
+    a = ingredient['amount']
+    m = ingredient['measurement']
+    t = ingredient['type']
+    d = ingredient['desc']
+    p = ingredient['prep']
+
+    # Altering Descriptors for ingredients
+    descriptions = transforms['Descriptions']
+    for item in descriptions:
+        for i in descriptions[item]:
+            if 'ingredient' in list(i.keys()) and (t.lower() in i['ingredient'] or i['ingredient'] in t.lower()):
+                if i['append'] == 'false':
+                    d = item
+                else:
+                    d = item + ' ' + d
+            if d != '' and 'description' in list(i.keys()) and (d.lower() in i['description'] or i['description'] in d.lower()):
+                if i['append'] == 'false':
+                    d = item
+                else:
+                    d = item + ' ' + d
+
+    # Substituting ingredients for other ingredients
+    substitutions = transforms['Substitutions']
+    for item in substitutions:
+        if t.lower() in substitutions[item]['Replacers']:
+            t = item
+            try:
+                d = substitutions[item]['Desc']
+            except:
+                pass
+            a = a*substitutions[item]['Ratio']
+
+    return{'amount': a, 'measurement': m, 'type': t, 'desc': d, 'prep': p}
